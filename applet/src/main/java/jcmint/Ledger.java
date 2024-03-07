08f742cd14ee747a63c1ee6a0f2366548468e512
Antonín Dufka <xdufka1@fi.muni.cz>
2024-03-07 14:46:15 +0100

    fix: linearize multi-dimensional arrays

--- applet/src/main/java/jcmint/Ledger.java
+++ applet/src/main/java/jcmint/Ledger.java
@@ -3,16 +3,16 @@ package jcmint;
 import javacard.framework.Util;
 
 public class Ledger {
-    private final byte[][] tokens = new byte[32][100];
+    private final byte[] tokens = new byte[32 * 100];
     short last = 0;
 
     public void append(byte[] data, short offset) {
-        Util.arrayCopyNonAtomic(data, offset, tokens[last++], (short) 0, (short) 32);
+        Util.arrayCopyNonAtomic(data, offset, tokens, (short) (last++ * 32), (short) 32);
     }
 
     public boolean contains(byte[] data, short offset) {
         for (short i = 0; i < last; ++i) {
-            if (Util.arrayCompare(data, offset, tokens[i], (short) 0, (short) 32) == 0)
+            if (Util.arrayCompare(data, offset, tokens, (short) (i * 32), (short) 32) == 0)
                 return true;
         }
         return false;
