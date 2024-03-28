9af3ec21c7cc0b92733b241f9b06e3a302f5c46a
Antonín Dufka <xdufka1@fi.muni.cz>
2024-03-28 12:54:23 +0100

    feat: add swap performance measurement

--- applet/src/test/java/tests/PerformanceTest.java
+++ applet/src/test/java/tests/PerformanceTest.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 
+import cz.muni.fi.crocs.rcard.client.Util;
 import org.bouncycastle.math.ec.ECPoint;
 import org.junit.jupiter.api.*;
 
@@ -37,6 +38,47 @@ public class PerformanceTest extends BaseTest {
             message = ProtocolManager.randomMessage(precomputed);
             hashedPoint = ProtocolManager.h2c(message);
             token = pm.swapSingle(previousMessage, token, hashedPoint, precomputed ? previousHashedPoint : null);
+            file.printf("%d\n", pm.cm.getLastTransmitTime());
+        }
+        file.close();
+    }
+
+    @Test
+    public void measureVerifySwap() throws Exception {
+        verifySwap(false, 1);
+    }
+
+    @Test
+    public void measureVerifySwapPrecomputed() throws Exception {
+        verifySwap(true, 1);
+    }
+
+    public void verifySwap(boolean precomputed, int parties) throws Exception {
+        PrintWriter file = new PrintWriter(new FileWriter(precomputed ? "verify_swap_precomputed.csv" : "verify_swap.csv", false));
+        ProtocolManager pm = new ProtocolManager(connect(), (byte) 0);
+
+        BigInteger[] privateKeys = new BigInteger[parties];
+        pm.setup(privateKeys);
+        byte[] message = ProtocolManager.randomMessage(precomputed);
+        ECPoint hashedPoint = ProtocolManager.h2c(message);
+        ECPoint token = pm.issue(hashedPoint);
+
+        for (int i = 1; i < privateKeys.length; ++i) {
+            token = token.add(hashedPoint.multiply(privateKeys[i]));
+        }
+
+        for (int i = 0; i < REPEAT; ++i) {
+            byte[] previousMessage = message;
+            ECPoint previousHashedPoint = hashedPoint;
+            byte[] proofs = pm.verify(previousMessage, token, precomputed ? previousHashedPoint : null);
+            file.printf("%d,", pm.cm.getLastTransmitTime());
+            message = ProtocolManager.randomMessage(precomputed);
+            hashedPoint = ProtocolManager.h2c(message);
+
+            for (int j = 1; j < privateKeys.length; ++j) {
+                proofs = Util.concat(proofs, ProtocolManager.computeProof(privateKeys[j], previousHashedPoint, ProtocolManager.G.multiply(privateKeys[j])));
+            }
+            token = pm.swap(previousMessage, token, hashedPoint, proofs);
             file.printf("%d\n", pm.cm.getLastTransmitTime());
         }
         file.close();
